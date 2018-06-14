3b4094deb47e08f089ed34c0da465ab97c077430
D7703_22 <d7703_22@D7703-22ui-iMac-2.local>
2018-06-14 11:35:13 +0900

    six red pin

--- Simple MapView/ViewController.swift
+++ Simple MapView/ViewController.swift
@@ -13,37 +13,78 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myMapView: MKMapView!
     
+    var foodStoreNames = ["늘해랑", "번개반점", "아딸", "왕짜장", "토마토도시락", "홍콩반점"]
+    var foodStoreAddress = ["부산광역시 부산진구 양정1동 350-1",
+                            "부산광역시 부산진구 양정동 418-282",
+                            "부산광역시 부산진구 양정동 393-18",
+                            "부산광역시 부산진구 양정1동 356-22",
+                            "부산광역시 부산진구 양정1동 350-1",
+                            "부산광역시 부산진구 양정동 353-38"]
+    var i = 0
+    // pin의 배열 생성 및 초기화
+    var annos  = [MKPointAnnotation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // 위도, 경도 설정
-        // 동의과학대학교
-        // 부산광역시 부산진구 양정동 429-19
-        // 35.165999, 129.07543
+        for item in foodStoreAddress{
+            let geocoder = CLGeocoder()
+            geocoder.geocodeAddressString(item) {
+                
+                //Error check
+                (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+                if let myError = error{
+                    print(myError)
+                    return
+                }
+                
+                let myPlacemark = placemarks![0]
+                //print(myPlacemark.location?.coordinate)
+                
+                // 한 개씩 뽑아서 지도에 핀 뽑기
+                if let loc = myPlacemark.location {
+                    let anno = MKPointAnnotation()
+                    anno.coordinate = loc.coordinate
+                    anno.title = self.foodStoreNames[self.i]
+                    anno.subtitle = self.foodStoreAddress[self.i]
+                    self.i = self.i+1
+                    
+                    // pin을 pin 배열에 추가
+                    self.annos.append(anno)
+                    self.myMapView.addAnnotations(self.annos)
+                    
+                    // pin을 맵에 꽉 채워서 보여줌
+                    self.myMapView.showAnnotations(self.annos, animated: true)
+                } else {
+                    print("loc nil 발생")
+                }
+            }
+        }
         
-        let center = CLLocationCoordinate2D (latitude: 35.165999, longitude: 129.07543)
+        let addr = "부산광역시 부산진구 양정동 418-282"
         
-        // 반경
-        let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+        let geoCoder = CLGeocoder()
         
-        // region
-        let region = MKCoordinateRegion(center: loc, span: span)
-        
-        // MapKit에 설정
-        myMapView.setRegion(region, animated: true)
-        
-        // annotation 설정
-        let anno = MKPointAnnotation()
-        anno.coordinate = loc
-        anno.title = "DIT 동의과학대학교"
-        anno.subtitle = "내 꿈이 자라는 곳"
-        myMapView.addAnnotation(anno)
-        
+        geoCoder.geocodeAddressString(addr) {
+            (placemarks: [CLPlacemark]?, error: Error?) -> Void in if let error = error {
+                print(error)
+                return
+            }
+            if let myPlacemarks = placemarks{
+                let placemark = myPlacemarks[0]
+                
+                let loc = placemark.location?.coordinate
+                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                let region = MKCoordinateRegion(center: loc!, span: span)
+                self.myMapView.setRegion(region, animated: true)
+                
+                //annotation
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = loc!
+                annotation.title = addr
+                self.myMapView.addAnnotation(annotation)
+            }
+        }
     }
-
-    
-
-
 }
 
